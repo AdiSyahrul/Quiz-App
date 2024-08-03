9badd7038e10ffa4f5c0b08f2b74d28d2d4304ad
AdiSyahrul <adisyahrul27@gmail.com>
2024-08-03 23:18:47 +0700

    styling landing page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,33 +16,44 @@ const Login = ({ onLogin }) => {
   };
 
   return (
-    <div className="login-container">
-      <div className="header">
-        <div className="text">Login</div>
+    <div className="page-container">
+      <div className="banner">
+        <h1>Quiz Web App</h1>
+        <p>
+        Quiz App is a simple React application that provides a quiz based on questions fetched from an external API. The app requires users to log in before they can start the quiz. It features user authentication, fetches quiz questions from an external API, includes a timer for quiz duration, and displays results after quiz completion.
+        </p>
+        <p className="footer-text">Created by Adi Syahrul Setya Budi</p>
       </div>
-      <div className="inputs">
-        <div className="content">
-          <img src="src/assets/username.png" alt="" />
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="content">
-          <img src="src/assets/key.png" alt="" />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-      </div>
-      <div className="submit-container">
-        <div className="submit" onClick={handleLogin}>
-          Login
+      <div className="login-side">
+        <div className="login-container">
+          <div className="header">
+            <div className="text">Login</div>
+          </div>
+          <div className="inputs">
+            <div className="content">
+              <img src="src/assets/username.png" alt="" />
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="content">
+              <img src="src/assets/key.png" alt="" />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="submit-container">
+            <div className="submit" onClick={handleLogin}>
+              Login
+            </div>
+          </div>
         </div>
       </div>
     </div>
